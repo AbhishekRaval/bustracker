1ec4e52feda49e28b06b1856afd8165258c6642e
nishchaycs <shah.nishc@husky.neu.edu>
2018-04-12 18:07:14 -0400

    Neasrest bus-stops accessed

--- lib/bustracker/fetch_json.ex
+++ lib/bustracker/fetch_json.ex
@@ -1,20 +1,26 @@
 defmodule Bustracker.Fetchjson do
-  def fetch(id) do
-    url(id)
+  def fetch(latitude, longitude) do
+    url(latitude, longitude)
     |> HTTPoison.get
     |> handle_response
+    |> extract
     |> IO.inspect
   end
-  def url(id) do
-    "https://api-v3.mbta.com/stops?filter[route_type]=3"
-  end
+
+  # def url(id) do
+  #   "https://api-v3.mbta.com/stops?filter[route_type]=3"
+  # end
 
   def url(latitude, longitude) do
-    "https://api-v3.mbta.com/stops?filter[latitude]="<>latitude<>"&filter[longitude]="<>longitude<>"&filter[radius]=0.01"
+    "https://api-v3.mbta.com/stops?filter[route_type]=3&filter[latitude]="<>Float.to_string(latitude)<>"&filter[longitude]="<>Float.to_string(longitude)<>"&filter[radius]=0.01"
+  end
+
+  def extract(maps) do
+    Enum.map(maps, fn (x) -> {x["id"], x["attributes"]["name"]} end)
   end
 
   def handle_response({:ok, %{status_code: 200, body: body}}) do
-    Poison.Parser.parse!(body)
-
+    temp = Poison.Parser.parse!(body)
+    temp["data"]
   end
 end
