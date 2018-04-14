17e5c7f67e8c888c362814e776a3bca555841844
nishchaycs <shah.nishc@husky.neu.edu>
2018-04-13 22:00:29 -0400

    bus list fetched

--- lib/bustracker/fetch_json.ex
+++ lib/bustracker/fetch_json.ex
@@ -15,8 +15,44 @@ defmodule Bustracker.Fetchjson do
     "https://api-v3.mbta.com/stops?filter[route_type]=3&filter[latitude]="<>Float.to_string(latitude)<>"&filter[longitude]="<>Float.to_string(longitude)<>"&filter[radius]=0.01"
   end
 
-  def extract(maps) do
-    Enum.map(maps, fn (x) -> {x["id"], x["attributes"]["name"]} end)
+  def fetch_buses(stopid) do
+    "https://api-v3.mbta.com/routes?filter[stop]="<>Integer.to_string(stopid)
+    |> HTTPoison.get
+    |> handle_response
+    |> extractRouteids
+    |> extractBuses
+    |> IO.inspect
+  end
+
+  def fetch_vehicleDetails(routeid) do
+    "https://api-v3.mbta.com/vehicles?filter[route]="<>routeid
+    |> HTTPoison.get
+    |> handle_response
+    |> extractHeadsign
+    |> IO.inspect
+  end
+
+  def fetch_headsign(tripid) do
+    tripmap = "https://api-v3.mbta.com/trips/"<>tripid
+              |> HTTPoison.get
+              |> handle_response
+    tripmap["attributes"]["headsign"]
+  end
+
+  defp extractHeadsign(vidlist) do
+    Enum.map(vidlist, fn (x) -> %{"id" => x["id"], "hs" => fetch_headsign(x["relationships"]["trip"]["data"]["id"])} end)
+  end
+
+  defp extractBuses(routeidlist) do
+    Enum.map(routeidlist, fn (x) -> fetch_vehicleDetails(x["id"]) end)
+  end
+
+  defp extractRouteids(routes) do
+    Enum.map(routes, fn (x) -> %{"id" => x["id"]} end)
+  end
+
+  defp extract(maps) do
+    Enum.map(maps, fn (x) -> %{"id" => x["id"], "name" => x["attributes"]["name"]} end)
   end
 
   def handle_response({:ok, %{status_code: 200, body: body}}) do
