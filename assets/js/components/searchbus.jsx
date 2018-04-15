fff94cc7ecd49b21c49bf258c8d0cc9a890b4afb
abhishek <abhishek.raval@gmail.com>
2018-04-15 19:58:45 -0400

    Cards

--- assets/js/components/searchbus.jsx
+++ assets/js/components/searchbus.jsx
@@ -64,7 +64,7 @@ export class SearchBus extends React.Component {
         }
         else return <div>
           <div className="d-flex h-100">
-            <div className="d-flex flex-column mx-auto py-1 w-100">
+            <div className="d-flex flex-column mx-auto py-2 w-100">
               <h3 className="row justify-content-center"><Button onClick={this.detectLocation}> Detect
               Location </Button></h3>
               <div className="row justify-content-center">
@@ -74,33 +74,29 @@ export class SearchBus extends React.Component {
                       return (
                         <AccordionItem title={busStop.name} expanded={busStop === 1} className="card accordiontitle"
                           key={busStop.id}>
-                          <div className="card-body">
-                            {`Buses at  BusStop ` + busStop.name}
-                            <div className="list-group">
-                              {//Loop the map of buses for given busstop
-                              }
-                              <Card>
-                                <CardHeader>
-                                  Your Individual Bus Details here
-                                </CardHeader>
-                                <CardBody>
-                                  <CardTitle>Bus Heading Towards</CardTitle>
-                                  <CardText> Arriving at : </CardText>
-                                  <CardText> Tack the bus</CardText>
-                                  <Button className="material-icons colorstar md48" color="none">star_border</Button>
-                                </CardBody>
-                              </Card>
+                          <div className=" card-body list-group">
+                            <Card>
+                              <CardHeader>
+                                Your Individual Bus Details here
+                              </CardHeader>
+                              <CardBody>
+                                <CardTitle>Bus Heading Towards</CardTitle>
+                                <CardText> Arriving at : </CardText>
+                                <CardText> Tack the bus</CardText>
+                                <Button className="material-icons colorstar md48" color="none">star_border</Button>
+                                <Button className="material-icons colorstar md48" color="none">star</Button>
+                              </CardBody>
+                            </Card>
 
-                              <Card>
-                                <CardHeader>
-                                  Your Individual Bus Details here
-                                </CardHeader>
-                                <CardBody>
-                                  <CardTitle className="text-center">Bus Title Here</CardTitle>
-                                  <CardText>abc</CardText>
-                                </CardBody>
-                              </Card>
-                            </div>
+                            <Card>
+                              <CardHeader>
+                                Your Individual Bus Details here
+                              </CardHeader>
+                              <CardBody>
+                                <CardTitle className="text-center">Bus Title Here</CardTitle>
+                                <CardText>abc</CardText>
+                              </CardBody>
+                            </Card>
                           </div>
                         </AccordionItem>);
                     })
