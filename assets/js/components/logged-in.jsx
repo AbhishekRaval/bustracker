9249bef357ea8ad7b3a80503085b101560ad8963
abhishekraval <abhishek.raval@gmail.com>
2018-04-18 22:07:29 -0400

    Map Integration

--- assets/js/components/logged-in.jsx
+++ assets/js/components/logged-in.jsx
@@ -5,7 +5,7 @@ import {SearchBus} from "./searchbus";
 import {Accordion, AccordionItem} from 'react-sanfona';
 import FavouriteView from './favourites';
 import BusTracking from './bus-tracking';
-
+import Map from './map';
 export default class LoggedIn extends React.Component {
 
   constructor(props) {
@@ -25,7 +25,7 @@ export default class LoggedIn extends React.Component {
           <div className="container2">
             <Nav name={this.props.profile.username} socket={this.props.session.socket} history={this.props.history} />
             <Route path="/" exact={true} render={() => {
-              return <div className="h-75 w-100">This should be the profile page </div>;
+                return <div className="h-25 w-25"><div className="row justify-content-center mt-2"><Map /></div></div>;
                 }}/>
                 <Route path="/favourites" exact={true} render={() => <FavouriteView />}/>
                 <Route path="/search" exact={true} render={() => <SearchBus channel={this.props.session.channel}
